7fd1617c6dff2b8a02b19d3c6b7f0256660cca9a
ssaraf <ssaraf@tavisca.com>
2017-08-23 14:31:53 +0530

    Library Management System

---
+++ LibraryManagementystem/LibraryManagementSystem_Fixtures/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LibraryManagementSystem_Fixtures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LibraryManagementSystem_Fixtures")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa83fadb-3aca-4ff4-8e0b-040aea49a177")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
